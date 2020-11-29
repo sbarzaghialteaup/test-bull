d454b955be1835d0732b469ddc8058bef6c5b3d3
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-11-29 23:38:23 +0100

    Utilizzo CPU

--- aggiungi-jobs.js
+++ aggiungi-jobs.js
@@ -6,7 +6,7 @@ async function main() {
     const jobs = Number(process.argv[2]);
     for (let index = 0; index < jobs; index++) {
         // eslint-disable-next-line no-await-in-loop
-        videoQueue.add({ video: "http://example.com/video1.mov", count: jobs });
+        videoQueue.add({ video: "http://example.com/video1.mov", index, count: jobs });
     }
     await videoQueue.close();
     console.log(`Jobs Aggiunti ${jobs}`);
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Queue = require("bull");
 
-const options = {
-    limiter: {
-        max: 10000,
-        duration: 1000,
-    },
-};
+const os = require("os");
+
 const videoQueue = new Queue("video transcoding");
 let startTime = 0;
 let count = 0;
 
+let startCpuUsage = {};
+let endCpuUsage = {};
+
 function startTimer() {
+    startCpuUsage = os.cpus();
     startTime = new Date();
 }
 
@@ -19,19 +19,51 @@ function getSecondsFromStart() {
     return now - startTime;
 }
 
-const PARALLEL_JOBS = 8;
-videoQueue.process(PARALLEL_JOBS, (job, done) => {
-    if (startTime === 0) {
-        startTimer();
+function calculateUsedCPU(usedTime) {
+    const CPUsUsage = [];
+
+    for (let cpu = 0; cpu < os.cpus().length; cpu++) {
+        const cpuUsage = {
+            index: cpu,
+            user: (endCpuUsage[cpu].times.user - startCpuUsage[cpu].times.user) / 10,
+        };
+
+        cpuUsage.userPerc = Math.trunc((100 * cpuUsage.user) / usedTime);
+
+        CPUsUsage.push(cpuUsage);
     }
-    count += 1;
-    done();
-    if (count === job.data.count) {
-        const usedTime = getSecondsFromStart();
-        console.log(
-            `Count ${count} Parallel jobs ${PARALLEL_JOBS} ${usedTime.toLocaleString()} milliseconds`
-        );
-        startTime = 0;
-        count = 0;
-    }
-});
+
+    const totalCpu = CPUsUsage.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.userPerc,
+        0
+    );
+
+    return totalCpu / CPUsUsage.length;
+}
+
+async function main(PARALLEL_JOBS) {
+    console.log(`Number of logical CPUs: ${os.cpus().length}`);
+    console.log(`CPU Speed: ${os.cpus()[0].speed}MHz`);
+    console.log();
+
+    videoQueue.process(PARALLEL_JOBS, (job, done) => {
+        if (startTime === 0) {
+            console.log(`Counting up to ${job.data.count} with ${PARALLEL_JOBS} parallel jobs`);
+            startTimer();
+        }
+        count += 1;
+        done();
+        if (count === job.data.count) {
+            const usedTime = getSecondsFromStart();
+            endCpuUsage = os.cpus();
+            const avgCPU = calculateUsedCPU(usedTime);
+            console.log(
+                `Count ${count} Parallel jobs ${PARALLEL_JOBS} ${usedTime.toLocaleString()} milliseconds - CPU ${avgCPU}%`
+            );
+            startTime = 0;
+            count = 0;
+        }
+    });
+}
+
+main(Number(process.argv[2]));

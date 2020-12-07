b03124198cfb1a83626129a0d34e5aa170124892
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-12-07 18:03:58 +0100

    docker

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -5,14 +5,23 @@ const Queue = require("bull");
 const { setQueues } = require("bull-board");
 
 const PORT = process.env.PORT || 8090;
+const REDIS_URI = process.env.REDIS_URI || "redis://127.0.0.1:6379";
 
 const videoQueues = new Map();
 
 async function main() {
+    console.log(REDIS_URI);
     for (let index = 0; index < 100; index++) {
         let customerName = `cliente-${index}-handlingMoved`;
         // eslint-disable-next-line no-await-in-loop
-        videoQueues.set(customerName, new Queue(customerName));
+        videoQueues.set(customerName, new Queue(customerName, REDIS_URI));
+
+        videoQueues.get(customerName).client.on("ready", () => {
+            console.log("Connesso a redis", customerName);
+        });
+        videoQueues.get(customerName).client.on("error", (error) => {
+            console.log("Errore connessione a redis", customerName, error);
+        });
 
         customerName = `cliente-${index}-residentTimes`;
         // eslint-disable-next-line no-await-in-loop
@@ -24,8 +33,10 @@ async function main() {
     }
 
     // eslint-disable-next-line no-restricted-syntax
-    for (const videoQueue of videoQueues.values()) {
-        setQueues([videoQueue]);
+    try {
+        setQueues([videoQueues.values()]);
+    } catch (error) {
+        console.error(error);
     }
 
     app.use("/", router);
